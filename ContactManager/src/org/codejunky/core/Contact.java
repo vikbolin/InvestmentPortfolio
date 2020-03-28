20bfc32603b012fb8b42510bc66c47e164c1563b
'Vikbol <'vikbol.in@gmail.com'>
2020-03-28 14:24:35 -0400

    Add class members

--- ContactManager/src/org/codejunky/core/Contact.java
+++ ContactManager/src/org/codejunky/core/Contact.java
@@ -1,5 +1,41 @@
 
 package org.codejunky.core;
 public class Contact {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+	private String email;
+	private String phone;
+	private String getFirstName() {
+		return firstName;
+	}
+	private void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	private String getLastName() {
+		return lastName;
+	}
+	private void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	private String getMiddleName() {
+		return middleName;
+	}
+	private void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	private String getEmail() {
+		return email;
+	}
+	private void setEmail(String email) {
+		this.email = email;
+	}
+	private String getPhone() {
+		return phone;
+	}
+	private void setPhone(String phone) {
+		this.phone = phone;
+	}
 
 }
